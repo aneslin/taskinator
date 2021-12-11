649f2cb7ca7480ea8f7ef72c8e90f89ba1dad9d7
Aaron Neslin <aneslin125@gmail.com>
2021-12-10 23:15:45 -0500

    added updating tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content"); 
 var tasks = []
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector('#tasks-completed')
 
 var completeEditTask = function(taskName, taskType, taskId){
   var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
@@ -18,6 +20,8 @@ alert("Task Updated!");
 formEl.removeAttribute("data-task-id");
 document.querySelector("#save-task").textContent = "Add task"
 };
+
+
 
 
 var taskFormHandler = function(event) { 
@@ -134,11 +138,29 @@ var taskButtonHandler = function(event){
     var taskId = event.target.getAttribute("data-task-id");
     deleteTask(taskId);
   };
+};
+
+var taskStatusChangeHandler = function(event) {
+  var taskId = event.target.getAttribute("data-task-id");
+
+  var statusValue = event.target.value.toLowerCase();
+
+var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+  if (statusValue === "to do"){
+    tasksToDoEl.appendChild(taskSelected);
+  }
+  else if (statusValue ===  "in progress"){
+    tasksInProgressEl.appendChild(taskSelected);
+  }
+
+  else if (statusValue === "completed"){
+    tasksCompletedEl.appendChild(taskSelected);
 }
 
-
+};
   formEl.addEventListener("submit", taskFormHandler);
   pageContentEl.addEventListener("click", taskButtonHandler);
 
-
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
 
